41681a71585fce7e2e164be98e894f82f6e68d1b
Siddhj2206 <siddhj2206@gmail.com>
2026-01-04 12:50:17 +0530

    Fixed the desktop file and dbus service

--- Casks/ghostty-linux.rb
+++ Casks/ghostty-linux.rb
@@ -1,3 +1,5 @@
+# Ghostty terminal emulator for Linux
+# Uses AppImage from pkgforge-dev/ghostty-appimage
 cask "ghostty-linux" do
   version "1.2.3"
   sha256 "cf239a0a9383aa9a148da2f6c6444993f871618cf4309d4db15d7be992d16725"
@@ -14,22 +16,35 @@ cask "ghostty-linux" do
 
   depends_on formula: "squashfs"
 
+  # Binary
   binary "squashfs-root/bin/ghostty"
+
+  # Shell completions
   bash_completion "squashfs-root/share/bash-completion/completions/ghostty.bash"
   fish_completion "squashfs-root/share/fish/vendor_completions.d/ghostty.fish"
   zsh_completion "squashfs-root/share/zsh/site-functions/_ghostty"
+
+  # Desktop integration
   artifact "squashfs-root/com.mitchellh.ghostty.desktop",
            target: "#{Dir.home}/.local/share/applications/com.mitchellh.ghostty.desktop"
   artifact "squashfs-root/com.mitchellh.ghostty.png",
            target: "#{Dir.home}/.local/share/icons/com.mitchellh.ghostty.png"
-  artifact "squashfs-root/share/nautilus-python/extensions/ghostty.py",
-           target: "#{Dir.home}/.local/share/nautilus-python/extensions/ghostty.py"
+  artifact "squashfs-root/share/dbus-1/services/com.mitchellh.ghostty.service",
+           target: "#{Dir.home}/.local/share/dbus-1/services/com.mitchellh.ghostty.service"
   artifact "squashfs-root/share/ghostty",
            target: "#{Dir.home}/.local/share/ghostty"
+
+  # File manager extensions (Nautilus for GNOME, KIO for KDE)
+  artifact "squashfs-root/share/nautilus-python/extensions/ghostty.py",
+           target: "#{Dir.home}/.local/share/nautilus-python/extensions/ghostty.py"
   artifact "squashfs-root/share/kio/servicemenus/com.mitchellh.ghostty.desktop",
            target: "#{Dir.home}/.local/share/kio/servicemenus/com.mitchellh.ghostty.desktop"
+
+  # Terminfo
   artifact "squashfs-root/share/terminfo/g/ghostty",
            target: "#{Dir.home}/.terminfo/g/ghostty"
+
+  # Vim syntax files
   artifact "squashfs-root/share/vim/vimfiles/compiler/ghostty.vim",
            target: "#{Dir.home}/.vim/compiler/ghostty.vim"
   artifact "squashfs-root/share/vim/vimfiles/ftdetect/ghostty.vim",
@@ -38,6 +53,8 @@ cask "ghostty-linux" do
            target: "#{Dir.home}/.vim/ftplugin/ghostty.vim"
   artifact "squashfs-root/share/vim/vimfiles/syntax/ghostty.vim",
            target: "#{Dir.home}/.vim/syntax/ghostty.vim"
+
+  # Neovim syntax files
   artifact "squashfs-root/share/nvim/site/compiler/ghostty.vim",
            target: "#{Dir.home}/.local/share/nvim/site/compiler/ghostty.vim"
   artifact "squashfs-root/share/nvim/site/ftdetect/ghostty.vim",
@@ -46,39 +63,57 @@ cask "ghostty-linux" do
            target: "#{Dir.home}/.local/share/nvim/site/ftplugin/ghostty.vim"
   artifact "squashfs-root/share/nvim/site/syntax/ghostty.vim",
            target: "#{Dir.home}/.local/share/nvim/site/syntax/ghostty.vim"
-  artifact "squashfs-root/share/dbus-1/services/com.mitchellh.ghostty.service",
-           target: "#{Dir.home}/.local/share/dbus-1/services/com.mitchellh.ghostty.service"
 
   preflight do
-    FileUtils.mkdir_p "#{Dir.home}/.local/share/dbus-1/services"
-    FileUtils.mkdir_p "#{Dir.home}/.local/share/applications"
-    FileUtils.mkdir_p "#{Dir.home}/.local/share/icons"
-    FileUtils.mkdir_p "#{Dir.home}/.local/share/nautilus-python/extensions"
-    FileUtils.mkdir_p "#{Dir.home}/.local/share/kio/servicemenus"
-    FileUtils.mkdir_p "#{Dir.home}/.local/share/nvim/site/compiler"
-    FileUtils.mkdir_p "#{Dir.home}/.local/share/nvim/site/ftdetect"
-    FileUtils.mkdir_p "#{Dir.home}/.local/share/nvim/site/ftplugin"
-    FileUtils.mkdir_p "#{Dir.home}/.local/share/nvim/site/syntax"
-    FileUtils.mkdir_p "#{Dir.home}/.terminfo/g"
-    FileUtils.mkdir_p "#{Dir.home}/.vim/compiler"
-    FileUtils.mkdir_p "#{Dir.home}/.vim/ftdetect"
-    FileUtils.mkdir_p "#{Dir.home}/.vim/ftplugin"
-    FileUtils.mkdir_p "#{Dir.home}/.vim/syntax"
+    # Create target directories for artifacts
+    dirs = [
+      "#{Dir.home}/.local/share/applications",
+      "#{Dir.home}/.local/share/dbus-1/services",
+      "#{Dir.home}/.local/share/icons",
+      "#{Dir.home}/.local/share/kio/servicemenus",
+      "#{Dir.home}/.local/share/nautilus-python/extensions",
+      "#{Dir.home}/.local/share/nvim/site/compiler",
+      "#{Dir.home}/.local/share/nvim/site/ftdetect",
+      "#{Dir.home}/.local/share/nvim/site/ftplugin",
+      "#{Dir.home}/.local/share/nvim/site/syntax",
+      "#{Dir.home}/.terminfo/g",
+      "#{Dir.home}/.vim/compiler",
+      "#{Dir.home}/.vim/ftdetect",
+      "#{Dir.home}/.vim/ftplugin",
+      "#{Dir.home}/.vim/syntax",
+    ]
+    dirs.each { |dir| FileUtils.mkdir_p(dir) }
 
+    # Extract AppImage
     appimage_path = "#{staged_path}/Ghostty-#{version}-x86_64.AppImage"
     system "chmod", "+x", appimage_path
     system appimage_path, "--appimage-extract", chdir: staged_path
     FileUtils.rm appimage_path
 
+    # Fix hardcoded build paths in extracted files
+    # The AppImage contains paths like /__w/ghostty-appimage/.../bin/ghostty
+    # that need to be replaced with the Homebrew prefix path
+    build_path_regex = %r{/__w/ghostty-appimage[^\s]*}
+
+    # Patch desktop file: fix paths and disable DBus activation (no systemd service)
     desktop_file = "#{staged_path}/squashfs-root/com.mitchellh.ghostty.desktop"
     desktop_content = File.read(desktop_file)
-    desktop_content.gsub!(%r{/__w/ghostty-appimage[^\s]*}, "#{HOMEBREW_PREFIX}/bin/ghostty")
+    desktop_content.gsub!(build_path_regex, "#{HOMEBREW_PREFIX}/bin/ghostty")
+    desktop_content.gsub!("DBusActivatable=true", "DBusActivatable=false")
     File.write(desktop_file, desktop_content)
 
+    # Patch DBus service: fix paths and remove systemd service reference
     dbus_service = "#{staged_path}/squashfs-root/share/dbus-1/services/com.mitchellh.ghostty.service"
     dbus_content = File.read(dbus_service)
-    dbus_content.gsub!(%r{/__w/ghostty-appimage[^\s]*}, "#{HOMEBREW_PREFIX}/bin/ghostty")
+    dbus_content.gsub!(build_path_regex, "#{HOMEBREW_PREFIX}/bin/ghostty")
+    dbus_content.gsub!(/^SystemdService=.*\n/, "")
     File.write(dbus_service, dbus_content)
+
+    # Patch Nautilus extension: use full path since Nautilus doesn't have Homebrew in PATH
+    nautilus_ext = "#{staged_path}/squashfs-root/share/nautilus-python/extensions/ghostty.py"
+    nautilus_content = File.read(nautilus_ext)
+    nautilus_content.gsub!("cmd = ['ghostty',", "cmd = ['#{HOMEBREW_PREFIX}/bin/ghostty',")
+    File.write(nautilus_ext, nautilus_content)
   end
 
   postflight do
@@ -87,7 +122,6 @@ cask "ghostty-linux" do
 
   zap trash: [
     "~/.cache/ghostty",
-    "~/.config/ghostty",
     "~/.local/share/applications/com.mitchellh.ghostty.desktop",
     "~/.local/share/dbus-1/services/com.mitchellh.ghostty.service",
     "~/.local/share/ghostty",
